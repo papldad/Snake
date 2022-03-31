ed675c53195fc4d2862c8c3b046c052f20108310
Pavel <pavel@example.com>
2022-03-31 22:05:59 +0300

    feat: add render squares

--- script.js
+++ script.js
@@ -1,102 +1,136 @@
-function init() {
+let viewModel = {
+	amountSquare: 400,
 
-	// ============================================
+	buttonStart: document.getElementById("83"),
+	buttonPause: document.getElementById("80"),
+	buttonLeft: document.getElementById("37"),
+	buttonRight: document.getElementById("39"),
+	buttonUp: document.getElementById("38"),
+	buttonDown: document.getElementById("40"),
 
-	console.log("init is ok.");
-	console.log("=================================");
+	buttonsControlLeft: document.getElementById("buttonsControlLeft"),
+	buttonsControlCenter: document.getElementById("buttonsControlCenter"),
+	buttonsControlRight: document.getElementById("buttonsControlRight"),
 
-	// ============================================
-	// 
-	// ============================================
+	scoreInfo: document.getElementById("scoreInfo"),
+	spanScore: document.getElementById("spanScore"),
 
-	let eventKey;
-	let activeKey;
+	startGameValue: false,
+	pauseGameValue: false,
+	controlButtonValue: false,
 
-	let startGameValue = false;
-	let buttonStart = document.getElementById("83");
-	let scoreInfo = document.getElementById("scoreInfo");
-	
-	let pauseGameValue;
-	let buttonPause = document.getElementById("80");
+	eventKey: null,
+	activeKey: null,
 
-	let assentPressButton = false;
-	let buttonLeft = document.getElementById("37");
-	let buttonRight = document.getElementById("39");
-	let buttonUp = document.getElementById("38");
-	let buttonDown = document.getElementById("40");
-
-	let buttonsControlLeft = document.getElementById("buttonsControlLeft");
-	let buttonsControlCenter = document.getElementById("buttonsControlCenter");
-	let buttonsControlRight = document.getElementById("buttonsControlRight");
-
-	function startGame() {
-		if (startGameValue) {
-			if (!pauseGameValue) {
-				pauseGame();
+	startGame: function () {
+		if (this.startGameValue) {
+			if (!this.pauseGameValue) {
+				this.pauseGame();
 			}
 			const restart = confirm("Restart?");
 			if (restart) {
 				document.location.reload();
 			}
 		} else {
-			startGameValue = true;
-			scoreInfo.style.display = "block";
-			buttonsControlLeft.style.display = "block";
-			buttonsControlCenter.style.display = "block";
-			buttonsControlRight.style.display = "block";
+			this.startGameValue = true;
+			this.scoreInfo.style.display = "block";
+			this.buttonsControlLeft.style.display = "block";
+			this.buttonsControlCenter.style.display = "block";
+			this.buttonsControlRight.style.display = "block";
 
-			buttonStart.innerHTML = "Restart";
+			this.buttonStart.innerHTML = "Restart";
 
-			assentPressButton = true;
+			this.controlButtonValue = true;
 
-			/*LOAD GAME()*/
+			gameModel.loadGame();
 		}
-	}
+	},
 
-	function pauseGame() {
-		if (pauseGameValue) {
-			pauseGameValue = false;
-			buttonPause.classList.remove("buttonActive");
-			buttonsControlLeft.style.display = "block";
-			buttonsControlRight.style.display = "block";
-			assentPressButton = true;
+	pauseGame: function () {
+		if (this.pauseGameValue) {
+			this.pauseGameValue = false;
+			this.buttonPause.classList.remove("buttonActive");
+			this.buttonsControlLeft.style.display = "block";
+			this.buttonsControlRight.style.display = "block";
+			this.controlButtonValue = true;
 		} else {
-			pauseGameValue = true;
-			buttonPause.classList.add("buttonActive");
-			buttonsControlLeft.style.display = "none";
-			buttonsControlRight.style.display = "none";
-			assentPressButton = false;
-			/*stopGame()*/
+			this.pauseGameValue = true;
+			this.buttonPause.classList.add("buttonActive");
+			this.buttonsControlLeft.style.display = "none";
+			this.buttonsControlRight.style.display = "none";
+			this.controlButtonValue = false;
+		}
+	},
+
+	pressConBut: function (controlButton) {
+		console.log(controlButton);
+		viewModel.eventKey = controlButton;
+
+		if (controlButton == 37) {
+			this.buttonLeft.classList.add("buttonActive");
+			this.buttonRight.classList.remove("buttonActive");
+			this.buttonUp.classList.remove("buttonActive");
+			this.buttonDown.classList.remove("buttonActive");
+		} else if (controlButton == 39) {
+			this.buttonLeft.classList.remove("buttonActive");
+			this.buttonRight.classList.add("buttonActive");
+			this.buttonUp.classList.remove("buttonActive");
+			this.buttonDown.classList.remove("buttonActive");
+		} else if (controlButton == 38) {
+			this.buttonLeft.classList.remove("buttonActive");
+			this.buttonRight.classList.remove("buttonActive");
+			this.buttonUp.classList.add("buttonActive");
+			this.buttonDown.classList.remove("buttonActive");
+		} else if (controlButton == 40) {
+			this.buttonLeft.classList.remove("buttonActive");
+			this.buttonRight.classList.remove("buttonActive");
+			this.buttonUp.classList.remove("buttonActive");
+			this.buttonDown.classList.add("buttonActive");
 		}
 	}
 
-	function pressConBut(controlButton) {
-		console.log(controlButton);
-		if (controlButton == 37) {
-			buttonLeft.classList.add("buttonActive");
-			buttonRight.classList.remove("buttonActive");
-			buttonUp.classList.remove("buttonActive");
-			buttonDown.classList.remove("buttonActive");
-		} else if (controlButton == 39) {
-			buttonLeft.classList.remove("buttonActive");
-			buttonRight.classList.add("buttonActive");
-			buttonUp.classList.remove("buttonActive");
-			buttonDown.classList.remove("buttonActive");
-		} else if (controlButton == 38) {
-			buttonLeft.classList.remove("buttonActive");
-			buttonRight.classList.remove("buttonActive");
-			buttonUp.classList.add("buttonActive");
-			buttonDown.classList.remove("buttonActive");
-		} else if (controlButton == 40) {
-			buttonLeft.classList.remove("buttonActive");
-			buttonRight.classList.remove("buttonActive");
-			buttonUp.classList.remove("buttonActive");
-			buttonDown.classList.add("buttonActive");
-		}
+}
+
+let gameModel = {
+	startSnakeLength: 3,
+
+	loadGame: function() {
+		console.log("LOAD GAME!");
+
+		viewModel.spanScore.innerHTML = this.startSnakeLength;
+	}
+}
+
+// ============================================
+// INIT
+// ============================================
+
+function init() {
+	// ============================================
+	// RENDER OF ARENA
+	// ============================================
+
+	const boxGame = document.getElementById("boxGame");
+	const w = document.getElementById("w");
+	const h = document.getElementById("h");
+	const { width, height } = boxGame.getBoundingClientRect();
+
+	w.innerHTML = width;
+	h.innerHTML = height;
+
+	for (let i = 1; i <= viewModel.amountSquare; i++) {
+
+		let div = document.createElement("div");
+		div.classList.add("squareEmpty");
+		div.textContent = i; // for count. this is temperarily!
+		boxGame.append(div);
+		console.log(i);
 	}
 
 	// ============================================
-	// BIND
+
+	// ============================================
+	// CONTROL
 	// ============================================
 
 	let buttons = document.querySelectorAll('button');
@@ -112,18 +146,16 @@ function init() {
 	});
 
 	function pressKey(key) {
-		eventKey = key;
-		if (eventKey === 83) {
-			startGame();
-		} else if (assentPressButton && (eventKey === 37 || eventKey === 39 || eventKey === 38 || eventKey === 40)) {
-			pressConBut(eventKey);
-		} else if (startGameValue && eventKey === 80) {
-			pauseGame();
+		if (key === 83) {
+			viewModel.startGame();
+		} else if (viewModel.controlButtonValue && (key === 37 || key === 39 || key === 38 || key === 40)) {
+			viewModel.pressConBut(key);
+		} else if (viewModel.startGameValue && key === 80) {
+			viewModel.pauseGame();
 		}
 	}
 
 	// ============================================
-
 }
 
 window.onload = init;
