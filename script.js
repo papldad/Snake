78d16e446a834aa3c019841fca65a9dc4ec7bf61
Pavel <pavel@example.com>
2022-04-01 14:36:30 +0300

    feat: add the info version

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const versionInfo = document.getElementById("versionInfo");
+
 let viewModel = {
+
 	boxGame: document.getElementById("boxGame"),
 	amountSquare: 400,
 	squareAll: document.getElementsByClassName("squareEmpty"),
@@ -36,6 +39,7 @@ let viewModel = {
 			}
 		} else {
 			this.startGameValue = true;
+			versionInfo.style.display = "none";
 			this.scoreInfo.style.display = "block";
 			this.buttonsControlLeft.style.display = "block";
 			this.buttonsControlCenter.style.display = "block";
@@ -211,11 +215,21 @@ let gameModel = {
 // ============================================
 
 function init() {
+	// ============================================
+	const version = "v1.1.7";
+	versionInfo.innerHTML = version;
+	// ============================================
+
+	// ============================================
+	// fix user-scalable=no for ios
+	// ============================================
 
 	document.addEventListener('touchmove', function (event) {
 		if (event.scale !== 1) { event.preventDefault(); }
 	}, false);
-	
+
+	// ============================================
+
 	// ============================================
 	// CONTROL
 	// ============================================
